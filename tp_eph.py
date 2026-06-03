ce75645f79339350ad2f0d0dd7fb181acb611c83
Barboza Matias Gabriel <barbozamatiasg.05@gmail.com>
2026-06-02 21:24:50 -0300

    agregado archivo .py para realizar analisis

---
+++ tp_eph.py
@@ -0,0 +1,5 @@
+# hacer el analisis dentro de este archivo
+import pandas as pd
+
+#usar este archivo csv, los demas .py es para que vean la obtencion de los datos, formateo de la inflacion y la creacion de este dataframe
+df = pd.read_csv("eph_limpio_con_ipc.csv")
